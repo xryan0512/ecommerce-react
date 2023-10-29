0eda3d2ebc8297ae242ec02e84837da9016d29dc
xryan0512 <bryandev12@gmail.com>
2023-10-29 18:46:46 -0400

    add delete items function from cart and switch items function

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,12 +4,45 @@ export const ShoppingCartContext = createContext()
 
 
 export const ShoppingCartProvider = ({ children }) => {
+    //Shopping Cart - Increment quantity
     const [counter, setCounter] = useState(0)
 
+    //Product Detail - open/close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    //Product detail - show product
+    const [productToShow, setProductToShow] = useState({})
+
+    //Shopping cart - Add product to cart
+    const [cartProducts, setCartProducts] = useState([])
+    //Shoping cart - Delete Product
+    const deleteCartProduct = (index) => {
+        let arr = cartProducts;
+        cartProducts.splice(index, 1)
+        setCartProducts([...arr])
+    }
+
+    const [optionSideMenu, setOptionSideMenu] = useState('')
+    const optSelectedSideMenu = (opt) => {
+        setIsProductDetailOpen(true)
+        setOptionSideMenu(opt)
+    }
     return (
         <ShoppingCartContext.Provider value={{
             counter,
-            setCounter
+            setCounter,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow,
+            cartProducts,
+            setCartProducts,
+            deleteCartProduct,
+            optSelectedSideMenu,
+            optionSideMenu
         }}>
             {children}
         </ShoppingCartContext.Provider>
